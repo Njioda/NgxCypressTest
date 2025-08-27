28e3ce399a34f9ad1a476a806eb03ec7c9ff1668
nnjioda@yahoo.fr <nnjioda@yahoo.fr>
2025-08-27 12:29:40 +0200

    all new code

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,20 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: false,
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/report",
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true
+  },
   e2e: {
     baseUrl: "http://localhost:4200",
     excludeSpecPattern: ["**/1-getting-started", "**/2-advanced-examples"],
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     }
   }
 });
